0702d47e176127a57ebad22bb8eb99cd70babc00
metalwp <qq47972087@gmail.com>
2020-07-21 18:02:13 +0800

    order_manager修改中V0.5

--- order_manager/views.py
+++ order_manager/views.py
@@ -89,22 +89,29 @@ def updateOrderData(request):
         quantity = request.POST.get('quantityUpdateInput')
         status = request.POST.get('statusUpdateSelect')
 
-        dilivery_str = request.POST.get('diliveryUpdateInput')
-        dilivery_time = datetime.datetime.strptime(dilivery_str, '%Y-%m-%d').date()
 
-
-        start_str = request.POST.get('startUpdateInput')
-        start_time = datetime.datetime.strptime(start_str, '%Y-%m-%d-%H:%M:%S')
-
-        end_str = request.POST.get('endUpdateInput')
-        end_time = datetime.datetime.strptime(end_str, '%Y-%m-%d-%H:%M:%S')
 
         try:
+            dilivery_time = None
+            dilivery_str = request.POST.get('diliveryUpdateInput')
+            if dilivery_str:
+                dilivery_time = datetime.datetime.strptime(dilivery_str, '%Y-%m-%d').date()
+
+            start_time = None
+            start_str = request.POST.get('startUpdateInput')
+            if start_str:
+                start_time = datetime.datetime.strptime(start_str, '%Y-%m-%d-%H:%M:%S')
+
+            end_time = None
+            end_str = request.POST.get('endUpdateInput')
+            if end_str:
+                end_time = datetime.datetime.strptime(end_str, '%Y-%m-%d-%H:%M:%S')
             mat, created = Order.objects.update_or_create(id=id, defaults={"order_status": status,
                                                                            "product_model": product, "quantity": quantity,
                                                                            "delivery_time": dilivery_time, "start_time": start_time,
                                                                            "end_time": end_time})
         except Exception as e:
+            print(e)
             return_dict = {"ret": False, "errMsg": str(e), "rows": [], "total": 0}
             return JsonResponse(return_dict)
 
@@ -114,14 +121,25 @@ def updateOrderData(request):
 
 class GetOrderNum(View):
 
-    def __init__(self):
-        super().__init__()
-        self.number = AutoSerialNumber()
-        self.number.timed_clear_serial_number()
+    # def __init__(self):
+    #     super().__init__()
 
     def get(self, request):
-        print(self.number, self.number())
-        return JsonResponse({})
+        return_dict = {"ret": True, "errMsg": '', "rows": [], "total": 0, "order_num": genOrderNum()}
+        return JsonResponse(return_dict)
+
+
+def genOrderNum():
+    """生成订单号"""
+    date_str = time.strftime("%Y%m%d", time.localtime(time.time()))
+    order = Order.objects.filter(order_no__contains="J" + date_str).order_by("-c_time").first()
+
+    if order:
+        serial_number = int(order.order_no[9:]) + 1
+    else:
+        serial_number = 1
+    order_number = "J" + date_str + "{0:03d}".format(serial_number)  # J20190612001
+    return order_number
 
 
 class AutoSerialNumber(object):
@@ -133,7 +151,7 @@ class AutoSerialNumber(object):
         self.order = Order.objects.filter(order_no__contains="J").order_by("-order_no").first()
         if self.order:
             self.fd_apply_no = self.order.order_no
-            #self.fd_apply_no = "J20196120001"
+            #self.fd_apply_no = "J20190612001"
             self.date_str = self.fd_apply_no[1: 9]  # 日期字符串
             self._serial_number = self.fd_apply_no[9:]  # 流水号字符串
             self._serial_number = 0  # 流水号
